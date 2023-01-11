5bd4b6844c406319eb5749bfe67967afb3489bb8
NotVeryFun <hank456s@gmail.com>
2023-01-11 22:41:59 +0800

    update image and tower ,bullet scripts

--- SlimeTD/Assets/Scripts/MapScript/TowerScript/Bullet.cs
+++ SlimeTD/Assets/Scripts/MapScript/TowerScript/Bullet.cs
@@ -6,19 +6,18 @@ public class Bullet : MonoBehaviour
 {
     public float lifespan;
     private float life;
-    private Camera cam;
+    public float bulletAtk;
+    public float bulletSpeed;
     Rigidbody2D rb;
-    Vector3 mousePos;
+    Vector3 enemyPos;
     void Start()
     {
         life = 0.0f;
-
-        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = mousePos - transform.position;
+        enemyPos = getNearestEnemyPos(transform.position);
+        Vector2 direction = enemyPos - transform.position;
        
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = direction.normalized;
+        rb.velocity = direction.normalized * bulletSpeed;
         
         float rotz = Mathf.Atan2(-direction.y,-direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0,0,rotz);
@@ -33,4 +32,27 @@ public class Bullet : MonoBehaviour
             Destroy(this.gameObject,0);
         }
     }
+    Vector3 getNearestEnemyPos(Vector3 pos){
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("MovingDude");
+        if(enemies.Length == 0)return new Vector3(0.0f,0.0f,0.0f);
+
+        Vector3 resPos = new Vector3(0.0f,0.0f,0.0f);
+        float minDis = float.MaxValue;
+        float dis = 0.0f;
+        foreach(GameObject g in enemies){
+            dis = getDisSquared(g.transform.position,pos);
+            if(dis < minDis){
+                minDis = dis;
+                resPos = g.transform.position;
+            }
+        }
+        return resPos;
+    }
+
+    float getDisSquared(Vector3 pos1,Vector3 pos2){
+        return (pos1.x - pos2.x) * (pos1.x - pos2.x) + (pos1.y - pos2.y) * (pos1.y - pos2.y) + (pos1.z - pos2.z) * (pos1.z - pos2.z);
+    }
+
+    
+
 }
--- SlimeTD/Assets/Scripts/MapScript/TowerScript/Tower.cs
+++ SlimeTD/Assets/Scripts/MapScript/TowerScript/Tower.cs
@@ -8,21 +8,52 @@ public class Tower : MonoBehaviour
     public float atkDamage;
     
     public GameObject bullet;
-    private Camera cam;
-    Vector3 mousePos;
+    Vector3 enemyPos;
+    float time;
     void Start()
     {
-        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        time = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 v = mousePos - transform.position;
+        enemyPos = getNearestEnemyPos(transform.position);
+        time += Time.deltaTime;
+        if(enemyPos == new Vector3(0.0f,0.0f,0.0f))return;
+        
+        Vector2 v = enemyPos - transform.position;
         float rotz = Mathf.Atan2(v.y,v.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0,0,rotz);
+        
 
-        Instantiate(bullet,transform.position,Quaternion.identity);
+        if(time >= atkSpeed){
+            time = 0.0f;
+            GameObject g = Instantiate(bullet,transform.position,Quaternion.identity);
+            g.GetComponent<Bullet>().bulletAtk = atkDamage;
+            
+        }
+    }
+
+
+    Vector3 getNearestEnemyPos(Vector3 pos){
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("MovingDude");
+        if(enemies.Length == 0)return new Vector3(0.0f,0.0f,0.0f);
+
+        Vector3 resPos = new Vector3(0.0f,0.0f,0.0f);
+        float minDis = float.MaxValue;
+        float dis = 0.0f;
+        foreach(GameObject g in enemies){
+            dis = getDisSquared(g.transform.position,pos);
+            if(dis < minDis){
+                minDis = dis;
+                resPos = g.transform.position;
+            }
+        }
+        return resPos;
+    }
+
+    float getDisSquared(Vector3 pos1,Vector3 pos2){
+        return (pos1.x - pos2.x) * (pos1.x - pos2.x) + (pos1.y - pos2.y) * (pos1.y - pos2.y) + (pos1.z - pos2.z) * (pos1.z - pos2.z);
     }
 }
