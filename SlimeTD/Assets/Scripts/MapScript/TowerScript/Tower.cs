124e251a838fd80d923e71f9cecf1b37e51e4ea2
NotVeryFun <hank456s@gmail.com>
2023-01-11 16:11:43 +0800

    shoot follows the mouse

--- SlimeTD/Assets/Scripts/MapScript/TowerScript/Tower.cs
+++ SlimeTD/Assets/Scripts/MapScript/TowerScript/Tower.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float atkSpeed;
+    public float atkDamage;
+    
+    public GameObject bullet;
+    private Camera cam;
+    Vector3 mousePos;
     void Start()
     {
-        
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 v = mousePos - transform.position;
+        float rotz = Mathf.Atan2(v.y,v.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0,0,rotz);
+
+        Instantiate(bullet,transform.position,Quaternion.identity);
     }
 }
