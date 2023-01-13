8ee12ed059ad355b817cf739ca11c1a2d644d82a
Bill <billwang1018@gmail.com>
2023-01-14 02:46:10 +0800

    add tower placing

--- SlimeTD/Assets/Scripts/Shop.cs
+++ SlimeTD/Assets/Scripts/Shop.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Tilemaps;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
+    BuildManager buildManager;
+    public TileBase towerA;
+    void Start() {
+        buildManager = BuildManager.instance;
+    }
     public void BuyStandardTurret()
     {
-        Debug.Log("Purchased Turret.");
+        if(buildManager.selectedBuilding != towerA) {
+            buildManager.selectedBuilding = towerA;
+            Debug.Log("Turret Selected.");
+        } else {
+            buildManager.selectedBuilding = null;
+            Debug.Log("Cancel Selection");
+        }
     }
 }
